04ecbc30b8991d6e2351c4a1c864b305522cf2e5
Jesper van den Ende <jespertheend@gmail.com>
2024-12-03 16:30:51 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-Deno.serve((req: Request) => new Response("Hello World"));
+#!/usr/bin/env -S deno run -A --watch --no-lock
+
+import {serveDir} from "jsr:@std/http/file-server";
+
+Deno.serve((req: Request) => {
+	return serveDir(req, {
+		showDirListing: true,
+		showIndex: true,
+	});
+});
